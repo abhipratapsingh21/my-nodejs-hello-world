df1286eb2adeb99bb4988aa3a0baff1e21d4372e
abhipratapsingh21 <abhipratapsingh.2109@gmail.com>
2024-10-11 19:53:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello RSA Team!");
+    response.end("Welcome to RSA World!");
 });
 
 const port = 80;
